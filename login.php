c27ad484c7f358e020b46a858b0924a1a03acb0b
FirstClassCoding <thanawat_gp@hotmail.com>
2022-12-04 16:52:02 +0700

    Create login.php

---
+++ login.php
@@ -0,0 +1,44 @@
+<html>
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
+
+    <head>
+        <title>Login Page</title>
+        <link rel = 'stylesheet' href = 'style.css'>
+    </head>
+
+    <body class = 'mainbg'>
+
+        <div class = 'register-box'>
+
+            <h2 class = 'head-text-box-register'>Login</h2>
+
+            <table class = 'set-position-register'>
+
+            <form method = 'post' action = ''>
+                <tr>
+                    <td class = 'set-text-register'>
+                        <label for = 'username'>Username</label>
+                    </td>
+                    <td>
+                        <input type = 'text' name = 'username'>
+                    </td>
+                </tr>
+                <tr>
+                    <td class = 'set-text-register'>
+                        <label for = 'password'>Password</lable>
+                    </td>
+                    <td>
+                        <input type = 'password' name = 'password'>
+                    </td>
+                </tr>
+            </table>
+
+                <input class = 'submit-register-bt-position' type = 'submit' value = 'Login'>
+                
+            </form>
+        </div>
+    </body>
+</html>
